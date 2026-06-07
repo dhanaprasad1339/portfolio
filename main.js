23b946f7ba03c908a6e39e41d651a0970674cf17
dhanaprasad1339 <dhanaprasad1339@gmail.com>
2026-06-06 21:42:04 -0700

    add glowring

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /*====================  toggle icon navbar ====================*/
-let menuIcon = document.querySelector('#meni-icon')
-let navbar = document.querySelector('navbar')
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
@@ -18,7 +18,7 @@ window.onscroll = () => {
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top <offset + height){
+        if(top >= offset && top < offset + height){
             navLinks.forEach(links =>{
                 links.classList.remove('active');
                 document.querySelector('header nav  a [ href*=' + id + ']').classList.add('active');
@@ -33,3 +33,22 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
+/*==================== scroll reveal ====================*/
+ScrollReveal({
+    //  reset: true,
+     distance:'80px',
+     duration:2000,
+     delay:200
+    });
+    ScrollReveal().reveal('.home-content,.heading', { origin: 'top' });
+    ScrollReveal().reveal('.home-img,.services-container,.portfolio-box,.contact form', { origin: 'bottom' });
+    ScrollReveal().reveal('.home-content h1,.about-img', { origin: 'left' });
+    ScrollReveal().reveal('.home-content p,.about-content', { origin: 'right' });
+    /*====================typed js ====================*/
+    const typed = new Typed('.multiple-text',{
+        strings:['Full Stack Developer','Data Analytics','Python Developer'],
+        typeSpeed:100,
+        backSpeed:100,
+        backDelay:1000,
+        loop:true
+    })
